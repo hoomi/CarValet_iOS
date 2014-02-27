6039f1ac3feb0ea1dbacd3c60302a4dae3aefefe
hoomi <hoomi2010@gmail.com>
2014-02-27 08:27:38 +0000

    Adds images with the same width

--- CarValet/CarImageViewController.m
+++ CarValet/CarImageViewController.m
@@ -19,7 +19,10 @@
 
 - (void) setupScrollContent
 {
-    NSMutableArray *imageViews = [NSMutableArray new];
+    CGFloat scrollWidth = self.scrollView.frame.size.width;
+    CGFloat totalWidth = scrollWidth * [carImagesArray count];
+    
+    UIView* carImageContainerView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, totalWidth,self.scrollView.frame.size.height)];
     
     CGFloat atX = 0.0;
     CGFloat maxHeight = 0.0;
@@ -28,19 +31,22 @@
     
     for (NSString *atCarImageName in carImagesArray) {
         carImage = [UIImage imageNamed:atCarImageName];
-        atImageView = [[UIImageView alloc] initWithImage:carImage];
-        atImageView.frame = CGRectMake(atX, 0.0, atImageView.bounds.size.width, atImageView.bounds.size.height);
-        [imageViews addObject:atImageView];
+        CGFloat scale = scrollWidth/carImage.size.width;
         
-        atX += atImageView.bounds.size.width;
-        if (atImageView.bounds.size.height > maxHeight) {
-            maxHeight = atImageView.bounds.size.height;
+        atImageView = [[UIImageView alloc] initWithImage:carImage];
+        CGFloat newHeight = atImageView.bounds.size.height * scale;
+        atImageView.frame = CGRectMake(atX, 0.0, scrollWidth, newHeight);
+        if (newHeight > maxHeight) {
+            maxHeight = newHeight;
         }
-    }
-    UIView* carImageContainerView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, atX,maxHeight)];
-    for (UIImageView *atImageView in imageViews) {
+        atX += scrollWidth;
         [carImageContainerView addSubview:atImageView];
     }
+    
+    CGRect newFrame =  carImageContainerView.frame;
+    newFrame.size.height = maxHeight;
+    carImageContainerView.frame = newFrame;
+
     [self.scrollView addSubview:carImageContainerView];
     self.scrollView.contentSize = carImageContainerView.bounds.size;
 }
