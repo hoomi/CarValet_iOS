5523eea62593e0a5decd71874951a715c984c492
hoomi <hoomi2010@gmail.com>
2014-03-06 08:22:09 +0000

    Moved the AboutView xib file to storyboard

--- CarValet/AppDelegate.m
+++ CarValet/AppDelegate.m
@@ -14,16 +14,16 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    UITabBarController *tabBarController = (UITabBarController*)self.window.rootViewController;
-    AboutViewController *aboutViewController = [[AboutViewController alloc]
-                                                initWithNibName:@"AboutViewController"
-                                                bundle:[NSBundle mainBundle]];
-    NSString *localizedAbout = NSLocalizedStringWithDefaultValue(@"About", nil, [NSBundle mainBundle], @"About", @"About tab title");
-    UITabBarItem *aboutTabItem = [[UITabBarItem alloc] initWithTitle:localizedAbout image:[UIImage imageNamed:@"info"] tag:0];
-    [aboutViewController setTabBarItem:aboutTabItem];
-    NSMutableArray *currentItems = [NSMutableArray arrayWithArray:tabBarController.viewControllers];
-    [currentItems addObject:aboutViewController];
-    [tabBarController setViewControllers:currentItems animated:NO];
+//    UITabBarController *tabBarController = (UITabBarController*)self.window.rootViewController;
+//    AboutViewController *aboutViewController = [[AboutViewController alloc]
+//                                                initWithNibName:@"AboutViewController"
+//                                                bundle:[NSBundle mainBundle]];
+//    NSString *localizedAbout = NSLocalizedStringWithDefaultValue(@"About", nil, [NSBundle mainBundle], @"About", @"About tab title");
+//    UITabBarItem *aboutTabItem = [[UITabBarItem alloc] initWithTitle:localizedAbout image:[UIImage imageNamed:@"info"] tag:0];
+//    [aboutViewController setTabBarItem:aboutTabItem];
+//    NSMutableArray *currentItems = [NSMutableArray arrayWithArray:tabBarController.viewControllers];
+//    [currentItems addObject:aboutViewController];
+//    [tabBarController setViewControllers:currentItems animated:NO];
     UIColor *mocha = [UIColor colorWithRed:128.0/255.0 green:64.0/255.0         // 1
                                       blue:0.0 alpha:1.0];
     UIColor *mochaPressed = [UIColor colorWithRed:128.0/255.0 green:64.0/255.0         // 1
@@ -31,6 +31,16 @@
     [[UIButton appearance] setTitleColor:mocha forState:UIControlStateNormal];
     [[UIButton appearance] setTitleColor:mochaPressed forState:UIControlStateSelected];
     [[UIBarButtonItem appearance] setTintColor:mocha];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    UITabBarController *tabController = (UITabBarController*)self.window.rootViewController;
+    // Get the last viewed scene (0 if there was none)
+    NSInteger lastScene = [defaults integerForKey:@"LastSceneShown"] ;
+    tabController.selectedIndex = lastScene;
+    
+     NSLog(@"didFinishLaunchingWithOptions --> The tab index was: %d",  lastScene);
+
     // Override point for customization after application launch.
     return YES;
 }
@@ -45,6 +55,20 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    UITabBarController *tabBarController = (UITabBarController*)self.window.rootViewController;
+    
+    // get the standard user defaults for this app
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    // set the preference for current scene
+    // (assumes currSceneType is set elsewhere to the id for the scene)
+    [defaults setInteger:tabBarController.selectedIndex forKey:@"LastSceneShown"];
+    
+    // make sure to synchronize so the defaults are saved
+    [defaults synchronize];
+    
+    NSLog(@"applicationDidEnterBackground --> The tab index was: %d",  tabBarController.selectedIndex);
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -59,7 +83,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
+    
+   }
 
 @end
